81709a839501d9923f2390a9279e3d48eebfd4e9
Mahksy <max.hausermaier@gmail.com>
2024-02-10 18:30:54 +0100

    switched to md5

--- interface.py
+++ interface.py
@@ -1,20 +1,23 @@
 from tkinter import *
 import customtkinter
+import sys
+import hashlib
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("green")
-
+customtkinter.deactivate_automatic_dpi_awareness()
 
 
 root = customtkinter.CTk()
 root.title("Hash calculator")
-root.geometry('400x200')
+root.geometry('300x200')
 root.grid_columnconfigure(0, weight=1)
 
 def gethash():
     tohash=myinput.get()
-    hashed=hash(tohash)
+    hashed=hashlib.md5(tohash.encode('utf-8')).hexdigest()
     myresult.configure(state="normal")
+    myresult.delete("1.0","end")
     myresult.insert("0.0", str(hashed))
     myresult.configure(state="disabled")    
 
@@ -30,8 +33,7 @@ mybutton.grid(row=2, column= 0, padx = 10, pady= 10, sticky ="ew")
 #mytextbox.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 #mytextbox.configure(state="disabled")
 
-myresult = customtkinter.CTkTextbox(root)
-myresult.configure(state="disabled")
+myresult = customtkinter.CTkTextbox(root, height=10)
 myresult.grid(row=3, column=0, padx = 10, pady= 10, sticky ="ew")
 
 
