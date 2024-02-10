dd52c36d34d17b8aaa21f14f876de57c6184dc1a
Mahksy <max.hausermaier@gmail.com>
2024-02-10 17:33:16 +0100

    added textbox

--- interface.py
+++ interface.py
@@ -12,8 +12,20 @@ root.geometry('400x200')
 
 root.grid_columnconfigure(0, weight=1)
 
+def gethash():
+    tohash=myinput.get()
+    hashed=hash(tohash)
+    
+
+
+
 myinput = customtkinter.CTkEntry(root, placeholder_text=("test"))
 myinput.grid(row=1, column =0, padx=10, pady=10, sticky="ew")
-mybutton = customtkinter.CTkButton(root, text='Calculate Hash')
-mybutton.grid(row=3, column= 0, padx = 10, pady= 10, sticky ="ew")
+
+mybutton = customtkinter.CTkButton(root, text='Calculate Hash', command=gethash)
+mybutton.grid(row=2, column= 0, padx = 10, pady= 10, sticky ="ew")
+
+mytextbox = customtkinter.CTkTextbox(root, height=20)
+mytextbox.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+
 root.mainloop()
